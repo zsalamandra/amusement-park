05c8f26ebbdeefbcf0ab0913dee8c1d2a07f7cfa
zsalamandra <zaur121079@gmail.com>
2025-01-02 17:20:38 +0300

    fix my bugs

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,33 +1,32 @@
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 
 @Getter
 public class Card {
 
     @Setter
-    private Integer balance = 0;
+    private BigDecimal balance = BigDecimal.ZERO;
 
     private final String validityPeriod;
 
-    private final Integer minCostForExit;
+    private final BigDecimal minCostForExit;
 
-    public Card(String validityPeriod, Integer minCostForExit) {
+    public Card(String validityPeriod, BigDecimal minCostForExit) {
 
         this.validityPeriod = validityPeriod;
         this.minCostForExit = minCostForExit;
     }
 
 
-    public void topUpBalance(Integer moneyAmount) {
+    public void topUpBalance(BigDecimal moneyAmount) {
 
-        if (moneyAmount < minCostForExit) {
-
+        if (moneyAmount.compareTo(minCostForExit) > 0) {
             throw new RuntimeException("Too Few");
-
         } else {
-
-            balance += moneyAmount;
+            balance.add(moneyAmount);
         }
     }
 }
